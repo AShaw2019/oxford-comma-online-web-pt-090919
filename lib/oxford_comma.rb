93810a42d0c02fa5c7e7a281b3339caa93e2430b
Ashley Twomey <aprier091012@gmail.com>
2019-09-18 23:39:15 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,5 @@
 def oxford_comma(array)
-  if  ["kiwi"].join(",")
-    return "kiwi"
-  end
-  elsif ["kiwi", "durian"].join("and")
-  return "kiwi and durian"
-end
-elsif ["kiwi", "durian", "starfruit"].join(", and")
-return "kiwi, durian, and starfruit"
+ 
 
   
  
